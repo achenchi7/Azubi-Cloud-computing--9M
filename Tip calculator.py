227dd1043fa1fa76e0e859a1ed78645a144a3e0a
Jully Achenchi <56290749+achenchi7@users.noreply.github.com>
2024-01-05 12:04:49 +0300

    Tip calculator.py

--- Tip calculator.py
+++ Tip calculator.py
@@ -1,8 +1,8 @@
-print ("Welcome to Tip calcilator")
+print ("Welcome to Tip calculator")
 Total_bill = float(input("What is the total bill?"))
-Tip_percentage = int("Percentage tip?")
+Tip_percentage = int(input("Percentage tip?"))
 Total_tip = Total_bill * (Tip_percentage/100)
-Sales_tax = int("Sales tax percentage?")
+Sales_tax = int(input("Sales tax percentage?"))
 Total_sales_tax = Total_bill * (Sales_tax/100)
 Grand_Total = Total_bill + Total_tip + Total_sales_tax
 
