d30e65fe1533b5011c6ef87ee538da09989f53db
Jully Achenchi <56290749+achenchi7@users.noreply.github.com>
2024-03-25 22:56:09 +0300

    variables.tf file

---
+++ terraform-assignment/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+    description = "Access key to the console"
+  
+}
+variable "aws_secret_key" {
+    description = "Secret key to the console"
+
+}
+
+
+# variable "s3_bucket_names" {
+#     type = list
+#     default = ["AdventureTech_JA_bucket", "NatureEscape_JA_bucket", "DataSummit_JA_bucket"]
+  
+# }
